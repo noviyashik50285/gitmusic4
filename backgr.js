94789c07f443ee1d250446aaf31fbefaa8233f9e
noviyashik50285 <mynewdisk50285@gmail.com>
2024-11-11 16:44:26 +0300

    mussec commit

---
+++ backgr.js
@@ -0,0 +1,55 @@
+const button0 = document.getElementById('butn0');
+button0.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara.jpg)';
+});
+const button1 = document.getElementById('butn1');
+button1.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara0.jpg)';
+});
+const button2 = document.getElementById('butn2');
+button2.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara1.jpg)';
+});
+const button3 = document.getElementById('butn3');
+button3.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara2.jpg)';
+});
+const button4 = document.getElementById('butn4');
+button4.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara3.jpg)';
+});
+const button5 = document.getElementById('butn5');
+button5.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara4.jpg)';
+});
+const button6 = document.getElementById('butn6');
+button6.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara5.jpg)';
+});
+const button7 = document.getElementById('butn7');
+button7.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara6.jpg)';
+});
+const button8 = document.getElementById('butn8');
+button8.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara7.jpg)';
+});
+const button9 = document.getElementById('butn9');
+button9.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara8.jpg)';
+});
+const button10 = document.getElementById('butn10');
+button10.addEventListener('click', function() {
+  const body = document.querySelector('body');
+  body.style.backgroundImage = 'url(back/niagara9.jpg)';
+});
---
+++ vklvikl3.js
@@ -0,0 +1,13 @@
+var b;
+function show_hide3()
+{
+if(b==1) {
+    document.getElementById("butnfon").style.display="inline";
+    return b=0;
+}
+else{
+    document.getElementById("butnfon").style.display="none";
+    return b=1;
+}
+}
+
