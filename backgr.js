1fac9520d7a84bc69431f0a2c55a94966b32403f
noviyashik50285 <mynewdisk50285@gmail.com>
2024-11-27 17:06:32 +0300

    first commit

--- backgr.js
+++ backgr.js
@@ -2,54 +2,90 @@ const button0 = document.getElementById('butn0');
 button0.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button1 = document.getElementById('butn1');
 button1.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara0.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button2 = document.getElementById('butn2');
 button2.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara1.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button3 = document.getElementById('butn3');
 button3.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara2.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button4 = document.getElementById('butn4');
 button4.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara3.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button5 = document.getElementById('butn5');
 button5.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara4.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button6 = document.getElementById('butn6');
 button6.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara5.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button7 = document.getElementById('butn7');
 button7.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara6.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button8 = document.getElementById('butn8');
 button8.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara7.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button9 = document.getElementById('butn9');
 button9.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara8.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
 const button10 = document.getElementById('butn10');
 button10.addEventListener('click', function() {
   const body = document.querySelector('body');
   body.style.backgroundImage = 'url(back/niagara9.jpg)';
+  obnulenie2();
+  this.style.color = 'blue';
 });
+
+var obnulenie2 = function(){
+  button0.style.color = 'red';
+  button1.style.color = 'red';
+  button2.style.color = 'red';
+  button3.style.color = 'red';
+  button4.style.color = 'red';
+  button5.style.color = 'red';
+  button6.style.color = 'red';
+  button7.style.color = 'red';
+  button8.style.color = 'red';
+  button9.style.color = 'red';
+  button10.style.color = 'red';
+}
